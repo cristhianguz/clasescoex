ad3b26012787d92a2c5f14b243b3d0c898880d61
CrisGuz <110140385+cristhianguz@users.noreply.github.com>
2023-02-10 09:46:46 -0500

    Add files via upload

---
+++ taller.js
@@ -0,0 +1,93 @@
+/* const num=3
+if(num==='3'){
+console.log("sisas")
+}else if(num===3){
+console.log('sisas2')
+}else{
+console.log('noks')
+}
+ */
+
+/* let edad= parseInt(prompt('ingrese su edad'))
+console.log(edad) */
+
+
+/* let numero1=parseInt(prompt('numero 1: '))
+let numero2=parseInt(prompt('numero 2: '))
+let numero3=parseInt(prompt('numero 3: '))
+if(numero1>numero2 & numero1>numero3){
+console.log('numero1 es mayor')
+}else if(numero2>numero3 & numero2>numero1){
+console.log('numero2 es mayor')
+}else if(numero3>numero1 & numero3>numero2){
+console.log('numero 3 es mayor')
+}
+if(numero1<numero2 & numero1<numero3){
+console.log('numero1 es menor')
+}else if(numero2<numero3 & numero2<numero1){
+console.log('numero2 es menor')
+}else if(numero3<numero1 & numero3<numero2){
+console.log('numero 3 es menor')
+} */
+
+
+/* let base=parseInt(prompt('ingrese el valor de la base: '))
+let altura=parseInt(prompt('ingrese el valor de la altura: '))
+if(base>=5 & altura>=4){
+ console.log(base*altura/2)
+} else if(base<5 & altura<4){
+ console.log(base*2+10)
+} */
+
+/* let numero=parseInt(prompt('ingrese numero entero: '))
+if(numero %2 ==0){
+ console.log('es par')
+}else{
+ console.log('impar')
+} */
+
+
+
+/* let nota=parseInt(prompt('ingrese su nota: '))
+if(nota===19 & nota===20){
+ console.log('su calificacion es A')
+}else if(nota>=16 & nota<=18){
+ console.log('su nota es B')
+}else if(nota>=13 & nota<=15){
+ console.log('su nota es C')
+}else if(nota>=10 & nota<=12){
+ console.log('su nota es D')
+}else if(nota>=1 & nota<=9){
+ console.log('su nota es E')
+}else{
+ console.log('error en tus notas ')
+} */
+
+/* 
+let num=parseInt(prompt('digite numero: '))
+let num2=parseInt(prompt('digite numero: '))
+if(num % num2==0){
+ console.log('son multiplos')
+}else{
+ console.log('no son multiplos')
+} */
+
+
+/* let num=parseInt(prompt('digite un numero: '))
+if(num>0){
+console.log('numero positivo')
+}else if(num<0){
+console.log('numero negativo')
+}else if(num===0){
+console.log('su numero es 0')
+} */
+
+/* let num1=parseInt(prompt('ingrese numero:'))
+let num2=parseInt(prompt('ingrese numero: '))
+let num3=parseInt(prompt('ingrese numero: '))
+let promedio=(num1+num2+num3)/3
+if (promedio>=5){
+console.log(true)
+}else{
+console.log(false)
+} */
